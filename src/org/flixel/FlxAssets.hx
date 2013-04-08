a71b8c6019c6c307c73d424eff7535d4c08022b6
Adam Wentz <floppya@gmail.com>
2013-04-08 15:40:51 -0500

    Use nme.Assets instead of nme.installer.Assets

--- src/org/flixel/FlxAssets.hx
+++ src/org/flixel/FlxAssets.hx
@@ -116,10 +116,10 @@ class FlxAssets
 	public static function cacheSounds():Void
 	{
 		#if android
-		Reflect.callMethod(nme.installer.Assets, "initialize", []);
+		Reflect.callMethod(nme.Assets, "initialize", []);
 		
-		var resourceClasses:Hash<Dynamic> = cast Reflect.getProperty(nme.installer.Assets, "resourceClasses");
-		var resourceTypes:Hash<String> = cast Reflect.getProperty(nme.installer.Assets, "resourceTypes");
+		var resourceClasses:Hash<Dynamic> = cast Reflect.getProperty(nme.Assets, "resourceClasses");
+		var resourceTypes:Hash<String> = cast Reflect.getProperty(nme.Assets, "resourceTypes");
 		
 		if (resourceTypes != null)
 		{
@@ -136,10 +136,10 @@ class FlxAssets
 	
 	public static function addBitmapDataToCache(key:String, bmd:BitmapData):Void
 	{
-		Reflect.callMethod(nme.installer.Assets, Reflect.field(nme.installer.Assets, "initialize"), []);
-		var resourceTypes:Map<String, String> = cast Reflect.getProperty(nme.installer.Assets, "resourceTypes");
+		Reflect.callMethod(nme.Assets, Reflect.field(nme.Assets, "initialize"), []);
+		var resourceTypes:Map<String, String> = cast Reflect.getProperty(nme.Assets, "resourceTypes");
 		
 		resourceTypes.set(key, "image");
-		nme.installer.Assets.cachedBitmapData.set(key, bmd);
+		nme.Assets.cachedBitmapData.set(key, bmd);
 	}
 }
